18d9d716fece90ef033134489303e99094479edb
Ippokratis Pantelidis <106438453+Ippo03@users.noreply.github.com>
2023-10-15 02:20:22 +0300

    Delete the-wild-oasis/services directory

--- the-wild-oasis/services/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabaseUrl = 'https://qvtjifrypqqckyjieuyn.supabase.co'
-const supabaseKey = 
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2dGppZnJ5cHFxY2t5amlldXluIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY3NjI1NjQsImV4cCI6MjAxMjMzODU2NH0.p04ay7GhaLHsqucViSeeHRZN28PcehdEhlrKgi0z_4k'
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase;
